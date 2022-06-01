da5e7ce9837b078cbd63f8266ba36be7d88075b8
Jenna Vlahos <Jenna_v@hotmail.co.uk>
2022-06-01 12:36:14 +0100

    finished access checks

--- car.test.js
+++ car.test.js
@@ -5,5 +5,13 @@ const myCar = new Car("BMW", 5000, "Turbo 300");
 describe('can access properties of car', () => {
     test('can access manufacturer', () => {
         expect(myCar.manufacturer).toBe("BMW");
-    })
+    });
+
+    test('can access price', () => {
+        expect(myCar.price).toBe(5000);
+    });
+
+    test('can access engine type', () => {
+        expect(myCar.engineType).toBe("Turbo 300");
+    });
 })
